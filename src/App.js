7fd95130fb056932f114ccba05ab6a742525c60b
Kelli Warner <kbwarner@gmail.com>
2020-05-17 16:31:59 -0400

    projects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
+//kbwarner.net
+
 const App = () => {
-  return <h1>Kelli's Portfolio</h1>;
+  return (
+    <div>
+      <h1>Kelli's Portfolio</h1>
+      <h2>Full Stack Web Developer</h2>
+      <h3>Projects</h3>
+      <h4>GOOD GAME | Full Stack Developer | Team Project | 2020</h4>
+      <a href="https://www.goodgameconnect.com">www.goodgameconnect.com</a>
+      <ul>
+        <li>
+          Mobile-first social media application that connects board game players
+        </li>
+        <li>
+          Worked in a team setting using Git branching workflow and Trello
+          project management
+        </li>
+        <li>
+          Technologies used: JavaScript, PostgreSQL, Node.js, Express, React,
+          CSS, Bootstrap, React Router, Google Location API, Heroku, Cloudinary
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
